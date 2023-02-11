bd91b59a4a2d5d363902ad54b26536ab8763fa3f
Kurosu9 <kurstan.abazbekov@alatoo.edu.kg>
2023-02-11 16:24:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import HomePage from "./page/HomePage";
+import ProfilePage from "./page/profilePage/ProfilePage";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      <ProfilePage />
       <Footer />
     </div>
   );
