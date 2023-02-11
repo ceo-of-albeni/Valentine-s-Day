4e76f77c7f2542ccefd846f7b48ba191ba86d0e2
Kurosu9 <kurstan.abazbekov@alatoo.edu.kg>
2023-02-12 01:23:03 +0600

    secont commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@ import React from "react";
 import Routing from "./Routing";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-<<<<<<<<< Temporary merge branch 1
-import UserContextProvider from "./context/userContext";
-
-const App = () => {
-  return (
-    <div className="d-flex flex-column min-vh-100">
-      <UserContextProvider>
-        <Navbar />
-        <Routing />
-        <Footer />
-      </UserContextProvider>
-=========
 import HomePage from "./page/HomePage";
 import ProfilePage from "./page/profilePage/ProfilePage";
 
@@ -23,7 +11,6 @@ const App = () => {
       <Navbar />
       <ProfilePage />
       <Footer />
->>>>>>>>> Temporary merge branch 2
     </div>
   );
 };
