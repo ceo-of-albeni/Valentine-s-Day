b6c5c05dfc012257e85eaf17dbc662be4f884066
Kurosu9 <kurstan.abazbekov@alatoo.edu.kg>
2023-02-12 18:28:10 +0600

    fifth commit

---
+++ src/components/modals/RedModal.js
@@ -0,0 +1,32 @@
+import React from "react";
+import classes from "./modals.module.css";
+import red from "../../images/red.svg"
+
+
+
+function RedModal({closeModal}) {
+    return (
+        <div className={classes.modal}>
+            <div className={classes.modal_container}>
+                <div className={classes.modal_inner}>
+                    <div className={classes.heart}>
+                        <img src={red} alt="heart"/>
+                        <button onClick={() => closeModal(false)}>X</button>
+                    </div>
+                    <div className={classes.form}>
+                        <form className={classes.form_bar}>
+                            <input className={classes.email} type="email" id="email" placeholder="Enter alatoo email"/>
+                            <textarea className={classes.text} id="freeform" name="textarea" placeholder="Type your text" rows="5" cols="40"/>
+                        </form>
+                    </div>
+                    <div className={classes.modal_footer}>
+                        <button onClick={() => closeModal(false)}>Cancel</button>
+                        <button type="submit">Send</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RedModal;
